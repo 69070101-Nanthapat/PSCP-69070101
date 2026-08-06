8fbedb161c3b8f397e381f6ac5035dd8ab2583cf
nanthapat <69070101@kmitl.ac.th>
2026-08-06 17:35:22 +0700

    3025

--- 3025/3025.py
+++ 3025/3025.py
@@ -3,14 +3,16 @@ def main():
     """Season"""
     month = int(input())
     day = int(input())
-    if day >= 21 and month in (3, 6, 9, 12):
-        month += 1
-    if month in (1, 2, 3):
+
+    if month == 12 and day >= 21 :
         print("winter")
-    elif month in (4, 5, 6):
-        print("spring")
-    elif month in (7, 8, 9):
-        print("summer")
-    elif month in (10, 11, 12):
+    elif month in (10, 11, 12) or (month == 9 and day >= 21):
         print("fall")
+    elif month in (7, 8, 9) or (month == 6 and day >= 21):
+        print("summer")
+    elif month in (4, 5, 6) or (month == 3 and day >= 21):
+        print("spring")
+    elif month in (1, 2, 3):
+        print("winter")
+
 main()
