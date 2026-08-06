e49d3c8482e94c0cf1d9f28860cc49c1040b5357
nanthapat <69070101@kmitl.ac.th>
2026-08-06 18:40:57 +0700

    3024

---
+++ 3024/3024.py
@@ -0,0 +1,14 @@
+"""1234"""
+def main():
+    """1234"""
+    total = float(input())
+    Max = float(input())
+    Min = total-Max-Max
+    if total <=3 and Max <= 2 and Min <0:
+        print("Not surprising")
+    elif Max-Min > 2:
+        print("Surprising")
+    else:
+        print("Not surprising")
+
+main()
